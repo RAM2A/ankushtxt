0a49fd7b30b08f4e073fe86ea5d33dc537563074
RAM2A <160558024+RAM2A@users.noreply.github.com>
2024-05-14 11:29:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
 from telethon import TelegramClient
-api_id = 24665357
-api_hash = "beb7e4b83ada668fa85f9a9b56338f1d"
-bot_token = "6700540841:AAEzEG75XEQXqfTGmIvy136zVclAUBxQKOI"
-skeleton_url = ""
 
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
+api_id = os.environ.get('API_ID', 24665357)
+api_hash = os.environ.get('API_HASH', "beb7e4b83ada668fa85f9a9b56338f1d")
+bot_token = os.environ.get('BOT_TOKEN', "6700540841:AAEzEG75XEQXqfTGmIvy136zVclAUBxQKOI")
 skeleton_url = os.environ.get('SKELETON_URL')
 
 
